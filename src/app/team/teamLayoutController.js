e27e158fe49683b425f5504be942dbb18c464dcd
Michael Dudley <michael.dudley@improvingenterprises.com>
2016-08-12 18:59:51 -0500

    fixing syntax

--- src/app/team/teamLayoutController.js
+++ src/app/team/teamLayoutController.js
@@ -1,12 +1,18 @@
-new functin(){
+new function() {
 
   mlm.package(this, {
-    name    ="team",
-    imports = "",
-    exports = "TeamLayoutController"
+    name:    "team",
+    imports: "miruken.mvc",
+    exports: "TeamLayoutController"
   });
 
   eval(this.imports);
+
+  const TeamLayoutController = Controller.extend({
+    constructor() {
+      
+    }
+  });
 
   eval(this.exports);
 };
