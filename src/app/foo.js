11862c594e80c703e146a1c35849637ff0cce192
Michael Dudley <michael.dudley@improvingenterprises.com>
2016-11-03 10:00:43 -0500

    these temp files are no longer needed'

--- src/app/foo.js
+++
@@ -1,3 +0,0 @@
-//import a from "./message";
-
-//console.log("*** " + a + " ***");
--- src/app/message.js
+++
@@ -1,5 +0,0 @@
-//export default "Default message"
-
-//export const message1 = "Hello, World!";
-
-//export const message2 = "Hola, World!";
